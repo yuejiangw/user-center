98dc01d6308d5bc6eac99cd5c07cdb960ec25b59
226wyj <308033240@qq.com>
2024-08-27 00:44:33 -0700

    添加 Register 单元测试，回滚脏数据

--- user-center-backend/src/test/java/com/yuejiangw/usercenterbackend/service/UserServiceTest.java
+++ user-center-backend/src/test/java/com/yuejiangw/usercenterbackend/service/UserServiceTest.java
@@ -1,31 +1,37 @@
 package com.yuejiangw.usercenterbackend.service;
 
+import com.yuejiangw.usercenterbackend.exception.CustomException;
 import com.yuejiangw.usercenterbackend.model.domain.User;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
+import static com.yuejiangw.usercenterbackend.service.Constants.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * 用户服务测试
  *
- * @author wu
+ * @author Wu
  */
 @SpringBootTest
+@Transactional
 class UserServiceTest {
 
     @Resource
-    private UserService userService;
+    UserService userService;
 
     @Test
-    void testAddUser() {
+    // @Rollback(value = false)
+    void testAddUser_successful() {
         User user = new User();
-        user.setUsername("user");
-        user.setUserAccount("123");
-        user.setAvatarUrl("www.test.com");
+        user.setUsername("username123");
+        user.setUserAccount("account123");
+        user.setAvatarUrl("www.avatar.com");
         user.setGender(0);
-        user.setUserPassword("pwd");
+        user.setUserPassword("password");
         user.setPhone("123");
         user.setEmail("test@email.com");
         user.setUserStatus(0);
@@ -36,40 +42,36 @@ class UserServiceTest {
     }
 
     @Test
-    void userRegister() {
-        String userAccount ;
-        String userPassword;
-        String checkPassword;
+    void testUserRegister_successful() {
+        long userId = userService.userRegister(VALID_ACCOUNT, VALID_PASSWORD, VALID_CHECK_PASSWORD);
+        assertNotEquals(-1, userId);
+    }
 
+    @Test
+    void testUserRegister_fail() {
         // 校验 userAccount 不为空
-        userAccount = "";
-        userPassword = "12345678";
-        checkPassword = "12345678";
-        long result = userService.userRegister(userAccount, userPassword, checkPassword);
-        assertEquals(-1, result);
+        assertThrows(CustomException.class, () -> {
+            userService.userRegister(INVALID_ACCOUNT_EMPTY, VALID_PASSWORD, VALID_CHECK_PASSWORD);
+        });
 
         // 校验 userAccount 长度不小于 4
-        userAccount = "te";
-        result = userService.userRegister(userAccount, userPassword, checkPassword);
-        assertEquals(-1, result);
-
-        // 校验 userPassword 和 checkPassword 长度不小于 8
-        userAccount = "test";
-        userPassword = "1234567";
-        result = userService.userRegister(userAccount, userPassword, checkPassword);
-        assertEquals(-1, result);
+        assertThrows(CustomException.class, () -> {
+            userService.userRegister(INVALID_ACCOUNT_LENGTH, VALID_PASSWORD, VALID_CHECK_PASSWORD);
+        });
 
         // 校验 userAccount 不能包含特殊字符
-        userAccount = "test 123";
-        userPassword = "12345678";
-        result = userService.userRegister(userAccount, userPassword, checkPassword);
-        assertEquals(-1, result);
+        assertThrows(CustomException.class, () -> {
+            userService.userRegister(INVALID_ACCOUNT_CHAR, VALID_PASSWORD, VALID_CHECK_PASSWORD);
+        });
+
+        // 校验 userPassword 和 checkPassword 长度不小于 8
+        assertThrows(CustomException.class, () -> {
+            userService.userRegister(VALID_ACCOUNT, INVALID_PASSWORD_LENGTH, INVALID_CHECK_PASSWORD_LENGTH);
+        });
 
         // 校验 userPassword 和 checkPassword 相等
-        userAccount = "test";
-        userPassword = "12345678";
-        checkPassword = "12345679";
-        result = userService.userRegister(userAccount, userPassword, checkPassword);
-        assertEquals(-1, result);
+        assertThrows(CustomException.class, () -> {
+            userService.userRegister(VALID_ACCOUNT, VALID_PASSWORD, INVALID_CHECK_PASSWORD_LENGTH);
+        });
     }
 }
