a14ca02589128ea5c6530b676b8ad4fbb5b4a0cc
226wyj <308033240@qq.com>
2024-08-27 00:50:25 -0700

    重构单元测试新增 Constants 类

---
+++ user-center-backend/src/test/java/com/yuejiangw/usercenterbackend/service/Constants.java
@@ -0,0 +1,19 @@
+package com.yuejiangw.usercenterbackend.service;
+
+public interface Constants {
+    String VALID_ACCOUNT = "account";
+
+    String VALID_PASSWORD = "password";
+
+    String VALID_CHECK_PASSWORD = "password";
+
+    String INVALID_ACCOUNT_EMPTY = "";
+
+    String INVALID_ACCOUNT_LENGTH = "acc";
+
+    String INVALID_ACCOUNT_CHAR = "#%$";
+
+    String INVALID_PASSWORD_LENGTH = "1234";
+
+    String INVALID_CHECK_PASSWORD_LENGTH = "1234";
+}
